393f24473c67452475f49921af3533d7ef4f16ea
pajp <pajp>
2004-05-08 00:53:51 +0000

    a class that cleans up the lyskom session when the http session invalidates

---
+++ nu/dll/app/weblatte/SessionWrapper.java
@@ -0,0 +1,23 @@
+import javax.servlet.http.*;
+import nu.dll.lyskom.Session;
+
+public class SessionWrapper implements HttpSessionBindingListener {
+    Session lyskom;
+    public SessionWrapper(Session lyskom) {
+	this.lyskom = lyskom;
+    }
+
+    public void valueBound(HttpSessionBindingEvent event) {
+	
+    }
+
+    public void valueUnbound(HttpSessionBindingEvent event) {
+	if (event.getName().equals("lyskom")) {
+	    lyskom.shutdown();
+	}
+    }
+
+    public Session getSession() {
+	return lyskom;
+    }
+}
