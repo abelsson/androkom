832540d0abd7deac56cc53d3597ac04e79cd22c3
pajp <pajp>
2004-06-04 14:40:28 +0000

    bugfix (absolute path since we are using Class.getResource() and not the one in ClassLoader)

--- nu/dll/app/weblatte/Version.java
+++ nu/dll/app/weblatte/Version.java
@@ -4,14 +4,18 @@ import nu.dll.lyskom.Debug;
 import java.io.*;
 
 public class Version {
-    static String version = "0.5";
+    static String resource = "/nu/dll/app/weblatte/version";
+    static String version = "0.0";
     static {
 	try {
-	    InputStream is = Version.class.getResourceAsStream("nu/dll/app/weblatte/cvs-version");
+	    InputStream is = Version.class.getResourceAsStream(resource);
 	    if (is != null) {
 		BufferedReader rdr = new BufferedReader(new InputStreamReader(is, "iso-8859-1"));
 		String row = rdr.readLine();
 		if (row != null) version = row;
+		else Debug.println("Version: resource " + resource + " is empty.");
+	    } else {
+		Debug.println("Version: resource " + resource + " not found.");
 	    }
 	} catch (IOException ex1) {
 	    ex1.printStackTrace();
