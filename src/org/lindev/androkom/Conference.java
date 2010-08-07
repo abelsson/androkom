512bb7d0e2d8543dc74c03696cde94eaec1eaf1d
Henrik Abelsson <henrik@abelsson.com>
2010-08-07 15:28:33 +0200

    Implement left and right flicks to move through text history, support scrolling of text longer than one page. This is still a bit buggy, but better than what was there before.

--- src/org/lindev/androkom/Conference.java
+++ src/org/lindev/androkom/Conference.java
@@ -1,16 +1,27 @@
 package org.lindev.androkom;
 
+import java.util.Stack;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Spanned;
+import android.text.method.Touch;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 /**
  * Show texts in a LysKOM conference.
@@ -18,8 +29,14 @@ import android.widget.TextView;
  * @author henrik
  *
  */
-public class Conference extends Activity 
+public class Conference extends Activity implements ViewSwitcher.ViewFactory
 {
+	
+
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
     /**
      * Set up activity. Will show individual LysKOM texts
      * with a click anywhere on the display moving to the 
@@ -30,41 +47,130 @@ public class Conference extends Activity
     {
         super.onCreate(savedInstanceState);
         
-        final Object data = getLastNonConfigurationInstance();
-        int confNo = (Integer) getIntent().getExtras().get("conference-id");
-               
-        Log.i("androkom", "Got passed conference id: " + confNo);
-        
+        setContentView(R.layout.conference);
+
+        mCurrentTextIndex = 0;
+
+        mSwitcher = (TextSwitcher)findViewById(R.id.flipper);
+        mSwitcher.setFactory(this);
+
+        mSlideLeftIn = AnimationUtils.loadAnimation(this, R.anim.slide_left_in);
+        mSlideLeftOut = AnimationUtils.loadAnimation(this, R.anim.slide_left_out);
+        mSlideRightIn = AnimationUtils.loadAnimation(this, R.anim.slide_right_in);
+        mSlideRightOut = AnimationUtils.loadAnimation(this, R.anim.slide_right_out);
         
 
-        final TextView tv = new TextView(this);
-        
-        if (data != null) {
-        	currentText = (String)data;
-        	tv.setText(Html.fromHtml(currentText), TextView.BufferType.SPANNABLE);
+        final Object data = getLastNonConfigurationInstance();
+        final int confNo = (Integer) getIntent().getExtras().get("conference-id");
+               
+        Log.i("androkom", "Got passed conference id: " + confNo);
+         
+        mCurrentText = new Stack<String>();
+        if (data != null) {      	
+        	mCurrentText.push((String)data);
+        	mSwitcher.setText(Html.fromHtml(mCurrentText.peek()));
+        	mCurrentTextIndex = 0;
         } else {        	
         	getApp().getKom().setConference(confNo);
-        	currentText = getApp().getKom().getNextUnreadText();
-        	tv.setText(Html.fromHtml(currentText), TextView.BufferType.SPANNABLE);
+        	mCurrentText.push(getApp().getKom().getNextUnreadText());
+        	
+        	Spanned text = Html.fromHtml(mCurrentText.peek());
+        	mCurrentTextIndex = 0;
+        	
+        	mSwitcher.setText(text);
         }
-        
-        setContentView(tv);
-        
-        tv.setOnClickListener(new OnClickListener() {       
-            public void onClick(View v) 
-            {               
-                currentText = getApp().getKom().getNextUnreadText();
-                tv.setText(Html.fromHtml(currentText), TextView.BufferType.SPANNABLE);
+ 
+        mGestureDetector = new GestureDetector(new MyGestureDetector());
+        mGestureListener = new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if (mGestureDetector.onTouchEvent(event)) {
+                    return true;
+                }
+                return false;
             }
-        });
+        };
         
+    }
+
+    class MyGestureDetector extends SimpleOnGestureListener {        
+
+        @Override
+        public boolean onScroll (MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+        {
+        	Log.i("androkom","got scroll event "+distanceX + " " + distanceY);
+        	if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH)
+        		return false;
+        	  	
+        	TextView widget = (TextView)mSwitcher.getCurrentView();
+
+        	// Constrain to top of text widget.
+        	int newX = Math.max((int)(widget.getScrollX()+distanceX),0);
+        	int newY = Math.max((int)(widget.getScrollY()+distanceY),0);
+            
+        	// TODO: Implement momentum scrolling.
+        	Touch.scrollTo(widget, widget.getLayout(), newX,  newY);
+
+			return true;       	
+        }
+		@Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                // right to left swipe
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                	
+                     
+                	mCurrentTextIndex++;
+                	Log.i("androkom","moving to next text cur:" + mCurrentTextIndex + "/" + mCurrentText.size()); 
+                	if (mCurrentTextIndex >= mCurrentText.size()) {
+                		Log.i("androkom", "fetching new text");
+                		mCurrentText.push(getApp().getKom().getNextUnreadText());                		
+                	}
+                		
+                	
+                	Spanned text = Html.fromHtml(mCurrentText.elementAt(mCurrentTextIndex));
+                                	
+                	mSwitcher.setInAnimation(mSlideLeftIn);
+                    mSwitcher.setOutAnimation(mSlideLeftOut);
+                    mSwitcher.setText(text);
+                    return true;
+                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                	Log.i("androkom","left swipe detected");
+                	mCurrentTextIndex--;        
+                    if (mCurrentTextIndex <= 0) {
+                    	mCurrentTextIndex = 0;
+                    	return true;
+                    }
+                	
+                    Spanned text = Html.fromHtml(mCurrentText.elementAt(mCurrentTextIndex));
+                    
+                	mSwitcher.setInAnimation(mSlideRightIn);
+                    mSwitcher.setOutAnimation(mSlideRightOut);
+                    mSwitcher.setText(text);
+                    return true;
+                }
+            } catch (Exception e) {
+                // nothing
+            }
+            return false;
+        }
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mGestureDetector.onTouchEvent(event))
+	        return true;
+	    else
+	    	return false;
     }
     
     @Override
     public Object onRetainNonConfigurationInstance() {    	
-    	return currentText;
+    	return mCurrentText;
     }
 
+   
     /**
      * Called when user has selected a menu item from the 
      * menu button popup. 
@@ -101,11 +207,35 @@ public class Conference extends Activity
         inflater.inflate(R.menu.conference, menu);
         return true;
     }
+    
+
+    /**
+     * Return TextViews for switcher.
+     */
+	public View makeView() {
+        TextView t = new TextView(this);
+        t.setText("[no text loaded]", TextView.BufferType.SPANNABLE);
+        t.setGravity(Gravity.TOP | Gravity.LEFT);
+        return t;
+	}
 
     App getApp() 
     {
         return (App)getApplication();
     }
     
-    private String currentText;
+    private int mCurrentTextIndex;
+    private Stack<String> mCurrentText;
+    
+    // For gestures and animations
+    
+    private GestureDetector mGestureDetector;
+    View.OnTouchListener mGestureListener;
+    private Animation mSlideLeftIn;
+    private Animation mSlideLeftOut;
+    private Animation mSlideRightIn;
+    private Animation mSlideRightOut;
+    private TextSwitcher mSwitcher;
+    
+    
 }
