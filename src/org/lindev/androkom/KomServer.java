174650c82f7be7bb491712ef03bab070f2cffdbb
Henrik Abelsson <henrik@abelsson.com>
2010-08-10 22:15:14 +0200

    Restore compatibility with API level 3 (Android 1.5).

--- src/org/lindev/androkom/KomServer.java
+++ src/org/lindev/androkom/KomServer.java
@@ -82,16 +82,6 @@ public class KomServer extends Service implements RpcEventListener, AsynchMessag
         }
     }
 
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
-        Log.i("LocalService", "Received start id " + startId + ": " + intent);
-        // We want this service to continue running until it is explicitly
-        // stopped, so return sticky.
-        return START_STICKY;
-    }
-
     @Override
     public IBinder onBind(Intent arg0) 
     {
