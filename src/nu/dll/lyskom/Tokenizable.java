b4e6d19483b38bbd5a4134869ea19ffb5b0f7c10
Rasmus Sten <rasmus@dll.nu>
2010-11-28 23:23:39 +0200

    deleted src/nu/dll/* and instead added lattekom.jar and added it to the classpath

--- src/nu/dll/lyskom/Tokenizable.java
+++
@@ -1,9 +0,0 @@
-package nu.dll.lyskom;
-
-/**
- * Indicates that this object can be converted into a KomToken by calling its
- * toToken() method.
- */
-public interface Tokenizable {
-	public KomToken toToken();
-}
