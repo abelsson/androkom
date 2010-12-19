9e78a15e6f8f06a05adc23ecb1713e1083df88ad
jonasgit <github@jonass.user.lysator.liu.se>
2010-12-19 20:16:27 +0100

    Merge from pajp

---
+++ nu/dll/lyskom/InternetHeaders.java
@@ -0,0 +1,28 @@
+package nu.dll.lyskom;
+
+import java.util.Enumeration;
+
+import android.util.Log;
+
+public class InternetHeaders {
+
+	private static final String TAG = "InternetHeaders";
+
+	public void setHeader(String name, String value) {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "setHeader:"+name+" : "+value);
+	}
+
+	public String getHeader(String string, Object object) {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "getHeader: "+string+" : "+object);
+		return null;
+	}
+
+	public Enumeration<Header> getAllHeaders() {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "getAllHeaders: ");
+		return null;
+	}
+
+}
