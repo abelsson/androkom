f1130d3c98add472c585951edb9846c6c0ea7e4c
Erik Pettersson <erik@ejp.se>
2011-07-10 21:45:29 +0200

    Use common AsyncTask to lookup recipients

--- src/org/lindev/androkom/Endast.java
+++ src/org/lindev/androkom/Endast.java
@@ -1,14 +1,15 @@
 package org.lindev.androkom;
 
 import nu.dll.lyskom.ConfInfo;
+
+import org.lindev.androkom.LookupRecipientTask.RunOnSuccess;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ComponentName;
-import android.content.DialogInterface;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,98 +21,76 @@ import android.widget.Toast;
  * @author jonas
  *
  */
-public class Endast extends Activity implements ServiceConnection
-{
-	public static final String TAG = "Androkom";
-	private KomServer mKom;
+public class Endast extends Activity implements ServiceConnection {
+    public static final String TAG = "Androkom";
+
+    private KomServer mKom;
+    private EditText mConfName;
+    private EditText mTexts;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) 
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.endast);
         getApp().doBindService(this);
         
+        mConfName = (EditText) findViewById(R.id.confname);
+        mTexts = (EditText) findViewById(R.id.num_texts);
         Button endastButton = (Button) findViewById(R.id.do_endast);
         endastButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) { doEndast(); }
+            public void onClick(View view) {
+                doEndast();
+            }
         });
-
     }
-    
+
     @Override
-    public void onDestroy()
-    {
-    	getApp().doUnbindService(this);
-    	super.onDestroy();
+    public void onDestroy() {
+        getApp().doUnbindService(this);
+        super.onDestroy();
     }
 
-	void doEndast() {
-		EditText confNameView = (EditText) findViewById(R.id.confname);
-		String confName = confNameView.getText().toString();
-		final ConfInfo[] confs = mKom.getConferences(confName);
+    void doEndast() {
+        final String confName = mConfName.getText().toString();
+        final String numTextsStr = mTexts.getText().toString();
+        final int numTexts;
 
-		EditText numTextsView = (EditText) findViewById(R.id.num_texts);
-		String textString = numTextsView.getText().toString();
-		final int texts = Integer.parseInt(textString);
+        try {
+            numTexts = Integer.valueOf(numTextsStr);
+        }
+        catch (final NumberFormatException e) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Not a number: " + numTextsStr);
+            builder.setPositiveButton("OK", null);
+            builder.create().show();
+            return;
+        }
+        if (numTexts < 0) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Number of texts must be positive");
+            builder.setPositiveButton("OK", null);
+            builder.create().show();
+            return;
+        }
 
-		if (texts > 0) {
-			if (confs.length == 1) {
-				Log.d(TAG, "doendast confname=" + confName);
-				Log.d(TAG, "doendast texts=" + texts);
-				mKom.endast(confs[0].getNo(), texts);
-				finish();
-			} else if (confs.length > 1) {
-				AlertDialog.Builder builder = new AlertDialog.Builder(
-						Endast.this);
-				builder.setTitle(getString(R.string.pick_a_name));
-				String[] vals = new String[confs.length];
-				for (int i = 0; i < confs.length; i++)
-					vals[i] = confs[i].getNameString();
-				builder.setSingleChoiceItems(vals, -1,
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int item) {
-								Toast.makeText(getApplicationContext(),
-										confs[item].getNameString(),
-										Toast.LENGTH_SHORT).show();
-								dialog.cancel();
-								int selectedConf = confs[item].getNo();
-								Log.d(TAG, "Selected user:"
-										+ selectedConf
-										+ ":"
-										+ new String(confs[item]
-												.getNameString()));
-								mKom.endast(selectedConf, texts);
-								finish();
-							}
-						});
-				AlertDialog alert = builder.create();
-				alert.show();
-			} else {
-				Toast.makeText(getBaseContext(), getString(R.string.no_conference_error),
-						Toast.LENGTH_SHORT).show();			
-			}
-		} else {
-			Toast.makeText(getBaseContext(), getString(R.string.no_zero_texts),
-					Toast.LENGTH_SHORT).show();
-		}
-	}
-    
-    App getApp() 
-    {
-        return (App)getApplication();
+        new LookupRecipientTask(this, mKom, confName, LookupRecipientTask.LOOKUP_CONFERENCES, new RunOnSuccess() {
+            public void run(final ConfInfo conf) {
+                Toast.makeText(getApplicationContext(), conf.getNameString(), Toast.LENGTH_SHORT).show();
+                mKom.endast(conf.getNo(), numTexts);
+                Endast.this.finish();
+            }
+        }).execute();
     }
-    
 
-	public void onServiceConnected(ComponentName name, IBinder service) {
-		mKom = ((KomServer.LocalBinder)service).getService();		
-	}
+    App getApp() {
+        return (App) getApplication();
+    }
 
-	public void onServiceDisconnected(ComponentName name) {
-		mKom = null;		
-	}
-	
-    private int selectedUser=0;
-    private EditText mConfName;
-    private EditText mTexts;
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        mKom = ((KomServer.LocalBinder)service).getService();
+    }
+
+    public void onServiceDisconnected(ComponentName name) {
+        mKom = null;
+    }
 }
